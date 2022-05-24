41d65669b83e9c2651b7d34490f4c4f36d525dc2
rvallely <68432429+rvallely@users.noreply.github.com>
2022-05-24 15:44:58 +0100

    add basic setup of app

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import Header from './Header';
+
+const Home = () => {
+    return (
+        <div>
+            <Header />
+            <p>Welcome to Discrete Maths exploration. Explore the Nav for options.</p>
+        </div>
+    ) 
+}
+
+export default Home;

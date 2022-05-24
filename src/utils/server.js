b505e461cd4c09cd927d7138f84f446db67b964e
rvallely <68432429+rvallely@users.noreply.github.com>
2022-05-24 23:53:45 +0100

    setup connection to server with two requests

---
+++ src/utils/server.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+const discreteMathsServer = axios.create({
+    baseURL: 'https://discrete-maths-exploration.herokuapp.com'
+});
+
+export const getMsg = () => {
+    return discreteMathsServer.get('/').then((data) => {
+        console.log(data.data)
+        return data.data.msg;
+    });
+}
+
+export const getBaseConversion = (value, fromBase, toBase) => {
+    console.log('in utils', value, fromBase, toBase);
+    const body = { val: value, fromBase: fromBase, toBase: toBase };
+    console.log('request obj >>', body)
+    return discreteMathsServer.post('/base-converter', body).then((data) => {
+        console.log(data.data)
+        return data.data.convertedValue;
+    });
+}

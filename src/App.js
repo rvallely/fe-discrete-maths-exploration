0805395d888f9b215dbc60f432d6e3a864d333c2
rvallely <68432429+rvallely@users.noreply.github.com>
2022-05-24 15:45:35 +0100

    add basic setup of app

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import { Routes, Route, BrowserRouter, Link } from "react-router-dom";
+import Home from './components/Home';
+import Nav from './components/Nav';
+import BaseConverter from './components/BaseConverter';
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className='App'>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/base-converter" element={<BaseConverter />} /> 
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;

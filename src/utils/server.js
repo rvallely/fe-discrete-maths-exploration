0e7a15c4aea418a6793bee08bfb72cfe2bad65c8
rvallely <68432429+rvallely@users.noreply.github.com>
2022-05-25 19:07:52 +0100

    remove console logs base-converter

--- src/utils/server.js
+++ src/utils/server.js
@@ -6,17 +6,13 @@ const discreteMathsServer = axios.create({
 
 export const getMsg = () => {
     return discreteMathsServer.get('/').then((data) => {
-        //console.log(data.data)
         return data.data.msg;
     });
 }
 
 export const getBaseConversion = (value, fromBase, toBase) => {
-   // console.log('in utils', value, fromBase, toBase);
     const body = { val: value, fromBase: fromBase, toBase: toBase };
-   // console.log('request obj >>', body)
     return discreteMathsServer.post('/base-converter', body).then((data) => {
-        console.log(data)
         return data.data.convertedValue;
     });
 }

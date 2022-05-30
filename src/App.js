98775fcde3899b3a242e7cf6290105db15aa9f1e
rvallely <68432429+rvallely@users.noreply.github.com>
2022-05-30 11:23:16 +0100

    tidy app.js

--- src/App.js
+++ src/App.js
@@ -3,20 +3,22 @@ import { Routes, Route, BrowserRouter, Link } from "react-router-dom";
 import Home from './components/Home';
 import Nav from './components/Nav';
 import BaseConverter from './components/BaseConverter/BaseConverter';
-
+import ShowCalculation from './components/BaseConverter/ShowCalculation';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <div className='App'>
-        <Nav />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/base-converter" element={<BaseConverter />} /> 
-        </Routes>
-      </div>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <div className='App'>
+                <Nav />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/base-converter" element={<BaseConverter />} /> 
+                    <Route path="/base-converter/show-calculation" element={<ShowCalculation />} /> 
+                </Routes>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
+// :startVal/:fromBase/:toBase/:result

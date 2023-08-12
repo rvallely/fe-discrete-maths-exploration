4bbe2c13b61b3f23e8c811dea1039cdda9c9b08a
rvallely <rosievallely@hotmail.com>
2023-08-12 17:05:08 +0100

    write sorting algorithm controllers

--- src/utils/server.js
+++ src/utils/server.js
@@ -4,15 +4,87 @@ const discreteMathsServer = axios.create({
     baseURL: 'https://discrete-maths-exploration.herokuapp.com'
 });
 
-export const getMsg = () => {
+export const getMsg = async () => {
     return discreteMathsServer.get('/').then((data) => {
         return data.data.msg;
     });
 }
 
-export const getBaseConversion = (value, fromBase, toBase) => {
-    const body = { val: value, fromBase: fromBase, toBase: toBase };
+export const getBaseConversion = async (value, fromBase, toBase) => {
+    const body = { val: value, fromBase, toBase };
+    console.log('making call to ', discreteMathsServer);
     return discreteMathsServer.post('/base-converter', body).then((data) => {
         return data.data;
     });
 }
+
+export const getSortedListUsingBubbleSort = async (unorderedNumberList) => {
+    return {
+        sortedNumberList: String(unorderedNumberList.sort((a, b) => a - b)).replace(/,/g, ', '),
+        sortingSteps: [
+            { iteration: [2, 7, 8, 10], swapNeeded: true},
+            { iteration: [2, 7, 8, 10], swapNeeded: false}
+        ],
+        timeTaken: '0.25',
+    }
+        // return discreteMathsServer.post('/sorting-algorithms/bubble-sort', body).then((data) => {
+    //     return data.data;
+    // });
+}
+
+export const getSortedListUsingBubbleSortFaster = async (unorderedNumberList) => {
+    return {
+        sortedNumberList: String(unorderedNumberList.replace(' ', '').split(',').map(num => Number(num)).sort((a, b) => a - b)).replace(/,/g, ', '),
+        sortingSteps: [
+            { iteration: [2, 7, 8, 10], swapNeeded: true},
+            { iteration: [2, 7, 8, 10], swapNeeded: false}
+        ],
+        timeTaken: '0.25',
+    }
+}
+
+export const getSortedListUsingInsertionSort = async (unorderedNumberList) => {
+    return {
+        sortedNumberList: String(unorderedNumberList.replace(' ', '').split(',').map(num => Number(num)).sort((a, b) => a - b)).replace(/,/g, ', '),
+        sortingSteps: [
+            { iteration: [2, 7, 8, 10], swapNeeded: true},
+            { iteration: [2, 7, 8, 10], swapNeeded: false}
+        ],
+        timeTaken: '0.25',
+    }
+}
+
+export const getSortedListUsingMergeSort = async (unorderedNumberList) => {
+    return {
+        sortedNumberList: String(unorderedNumberList.replace(' ', '').split(',').map(num => Number(num)).sort((a, b) => a - b)).replace(/,/g, ', '),
+        sortingSteps: [
+            { iteration: [2, 7, 8, 10], swapNeeded: true},
+            { iteration: [2, 7, 8, 10], swapNeeded: false}
+        ],
+        timeTaken: '0.25',
+    }
+}
+
+export const getSortedListUsingQuickSort = async (unorderedNumberList) => {
+    return {
+        sortedNumberList: String(unorderedNumberList.replace(' ', '').split(',').map(num => Number(num)).sort((a, b) => a - b)).replace(/,/g, ', '),
+        sortingSteps: [
+            { iteration: [2, 7, 8, 10], swapNeeded: true},
+            { iteration: [2, 7, 8, 10], swapNeeded: false}
+        ],
+        timeTaken: '0.25',
+    }
+}
+
+export const getSortedListUsingSelectionSort = async (unorderedNumberList) => {
+    return {
+        sortedNumberList: String(unorderedNumberList.replace(' ', '').split(',').map(num => Number(num)).sort((a, b) => a - b)).replace(/,/g, ', '),
+        sortingSteps: [
+            { iteration: [2, 7, 8, 10], swapNeeded: true},
+            { iteration: [2, 7, 8, 10], swapNeeded: false}
+        ],
+        timeTaken: '0.25',
+    }
+}
+
+

25aa7a2832f5f17d0c046c3366aa8f4e97489dd2
rvallely <rosievallely@hotmail.com>
2023-08-14 04:21:26 +0100

    add env file

--- src/utils/server.js
+++ src/utils/server.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const discreteMathsServer = axios.create({
-    baseURL: 'https://discrete-maths-exploration.herokuapp.com'
+    baseURL: process.env === 'prod' ? 'https://discrete-maths-exploration.herokuapp.com': 'http://localhost:9090/',
+    headers: {'Access-Control-Allow-Origin': '*'}
 });
 
 export const getMsg = async () => {
@@ -14,22 +15,23 @@ export const getBaseConversion = async (value, fromBase, toBase) => {
     const body = { val: value, fromBase, toBase };
     console.log('making call to ', discreteMathsServer);
     return discreteMathsServer.post('/base-converter', body).then((data) => {
+        console.log('data: ', data)
         return data.data;
     });
 }
 
-export const getSortedListUsingBubbleSort = async (unorderedNumberList) => {
-    return {
-        sortedNumberList: String(unorderedNumberList.sort((a, b) => a - b)).replace(/,/g, ', '),
-        sortingSteps: [
-            { iteration: [2, 7, 8, 10], swapNeeded: true},
-            { iteration: [2, 7, 8, 10], swapNeeded: false}
-        ],
-        timeTaken: '0.25',
-    }
-        // return discreteMathsServer.post('/sorting-algorithms/bubble-sort', body).then((data) => {
-    //     return data.data;
-    // });
+export const getSortedListUsingBubbleSort = async (unsortedList) => {
+    // return {
+    //     sortedNumberList: String(unorderedNumberList.sort((a, b) => a - b)).replace(/,/g, ', '),
+    //     sortingSteps: [
+    //         { iteration: [2, 7, 8, 10], swapNeeded: true},
+    //         { iteration: [2, 7, 8, 10], swapNeeded: false}
+    //     ],
+    //     timeTaken: '0.25',
+    // }
+    return discreteMathsServer.post('/sorting-algorithms/bubble-sort', { unsortedList }).then((data) => {
+        return data.data;
+    });
 }
 
 export const getSortedListUsingBubbleSortFaster = async (unorderedNumberList) => {

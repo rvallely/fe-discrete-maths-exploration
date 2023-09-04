7e13c10d70b374bdb639e0b4739845885bede1ae
rvallely <rosievallely@hotmail.com>
2023-09-04 13:31:14 +0100

    make more DRY friendly

--- src/components/SortingAlgorithms/SortingAlgorithmProcess.jsx
+++ src/components/SortingAlgorithms/SortingAlgorithmProcess.jsx
@@ -1,108 +1,75 @@
+/* eslint-disable max-len */
+/* eslint-disable key-spacing */
 import React from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import SortingAlgorithmType from '../../types/SortingAlgorithm';
 import QuickSortProcess from './QuickSortProcess';
 
 function renderCorrectAlgorithmProcessDisplay(sortingAlgorithmName, returnedApiData) {
-  console.log(returnedApiData.iterations[0], '<<< first element iterations');
+  let correctAlgorithmProcessDisplay;
   switch (sortingAlgorithmName) {
     case SortingAlgorithmType.BUBBLE_SORT.name:
-      return (
-        <div>
-          <h4>
-            Start: [
-            {String(returnedApiData.unsortedList)}
-            ]
-          </h4>
-          <table className="table">
-            <thead>
-              <tr key="bubble-sort-process-columns">
-                <th scope="col">Pass</th>
-                <th scope="col">List After Pass</th>
-                <th scope="col">Swap Needed</th>
+      correctAlgorithmProcessDisplay = (
+        <table className="table table-bordered table-responsive sorting-algorithm-process-table">
+          <thead className="table-active">
+            <tr key="bubble-sort-process-columns">
+              <th scope="col">Pass</th>
+              <th scope="col">List After Pass</th>
+              <th scope="col">Swap Needed</th>
+            </tr>
+          </thead>
+          <tbody>
+            {returnedApiData.iterations.map((step, index) => (
+              <tr key={uuidv4()}>
+                <td>{index + 1}</td>
+                <td>
+                  {JSON.stringify(step.listAfterPass)}
+                </td>
+                <td>{String(step.swapNeeded)}</td>
               </tr>
-            </thead>
-            <tbody>
-              {returnedApiData.iterations.map((step, index) => (
-                <tr key={uuidv4()}>
-                  <td>{index + 1}</td>
-                  <td>
-                    [
-                    {String(step.listAfterPass)}
-                    ]
-                  </td>
-                  <td>{String(step.swapNeeded)}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-          <h4>
-            End: [
-            {String(returnedApiData.sortedList)}
-            ]
-          </h4>
-        </div>
+            ))}
+          </tbody>
+        </table>
       );
+      break;
     case SortingAlgorithmType.BUBBLE_SORT_FASTER.name:
-      return (
-        <div>
-          <h4>
-            Start: [
-            {String(returnedApiData.unsortedList)}
-            ]
-          </h4>
-          <table className="table">
-            <thead>
-              <tr>
-                <th scope="col">Pass</th>
-                <th scope="col">List Before Pass</th>
-                <th scope="col">Swap Needed</th>
-                <th scope="col">List After Pass</th>
-                <th scope="col">Sorted List</th>
+      correctAlgorithmProcessDisplay = (
+        <table className="table table-bordered table-responsive sorting-algorithm-process-table">
+          <thead className="table-active">
+            <tr>
+              <th scope="col">Pass</th>
+              <th scope="col">List Before Pass (inputList)</th>
+              <th scope="col">Swap Needed</th>
+              <th scope="col">List After Pass</th>
+              <th scope="col">Sorted List (sortedList)</th>
+            </tr>
+          </thead>
+          <tbody>
+            {returnedApiData.iterations.map((step, index) => (
+              <tr key={uuidv4()}>
+                <td>{index + 1}</td>
+                <td>
+                  {JSON.stringify(step.listBeforePass)}
+                </td>
+                <td>{String(step.swapNeeded)}</td>
+                <td>
+                  {JSON.stringify(step.listAfterPass)}
+                </td>
+                <td>
+                  {JSON.stringify(step.sortedList)}
+                </td>
               </tr>
-            </thead>
-            <tbody>
-              {returnedApiData.iterations.map((step, index) => (
-                <tr key={uuidv4()}>
-                  <td>{index + 1}</td>
-                  <td>
-                    [
-                    {String(step.listBeforePass)}
-                    ]
-                  </td>
-                  <td>{String(step.swapNeeded)}</td>
-                  <td>
-                    [
-                    {String(step.listAfterPass)}
-                    ]
-                  </td>
-                  <td>
-                    [
-                    {String(step.sortedList)}
-                    ]
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-          <h4>
-            End: [
-            {String(returnedApiData.sortedList)}
-            ]
-          </h4>
-        </div>
+            ))}
+          </tbody>
+        </table>
       );
+      break;
     case SortingAlgorithmType.MERGE_SORT.name:
-      return (
+      correctAlgorithmProcessDisplay = (
         <div>
-          <h4>
-            Start: [
-            {String(returnedApiData.unsortedList)}
-            ]
-          </h4>
           <h5>Splitting Process</h5>
-          <table className="table">
-            <thead>
+          <table className="table table-bordered">
+            <thead className="table-active">
               <tr>
                 <th scope="col">Pass</th>
                 <th scope="col">List</th>
@@ -119,8 +86,8 @@ function renderCorrectAlgorithmProcessDisplay(sortingAlgorithmName, returnedApiD
           </table>
           <br key={uuidv4} />
           <h5>Sorting Process</h5>
-          <table className="table">
-            <thead>
+          <table className="table table-bordered">
+            <thead className="table-active">
               <tr>
                 <th scope="col">Pass</th>
                 <th scope="col">List</th>
@@ -135,122 +102,103 @@ function renderCorrectAlgorithmProcessDisplay(sortingAlgorithmName, returnedApiD
               ))}
             </tbody>
           </table>
-          <h4>
-            End: [
-            {String(returnedApiData.sortedList)}
-            ]
-          </h4>
         </div>
       );
+      break;
     case SortingAlgorithmType.QUICK_SORT.name:
-      return <QuickSortProcess iterations={returnedApiData.iterations} />;
+      correctAlgorithmProcessDisplay = (
+        <QuickSortProcess iterations={returnedApiData.iterations} />
+      );
+      break;
     case SortingAlgorithmType.INSERTION_SORT.name:
-      return (
-        <div>
-          <h4>
-            Start: [
-            {String(returnedApiData.unsortedList)}
-            ]
-          </h4>
-          <table className="table">
-            <thead>
-              <tr>
-                <th scope="col">Pass</th>
-                <th scope="col">List Before Pass</th>
-                <th scope="col">Value Being Inserted</th>
-                <th scope="col">List After Removing Value Being Inserted</th>
-                <th scope="col">Internal Process</th>
-                <th scope="col">List After Pass</th>
+      correctAlgorithmProcessDisplay = (
+        <table className="table table-bordered">
+          <thead className="table-active">
+            <tr>
+              <th scope="col">Pass</th>
+              <th scope="col">List Before Pass</th>
+              <th scope="col">Value Being Inserted</th>
+              <th scope="col">List After Removing Value Being Inserted</th>
+              <th scope="col">Internal Process</th>
+              <th scope="col">List After Pass</th>
+            </tr>
+          </thead>
+          <tbody>
+            {returnedApiData.iterations.map((step, index) => (
+              <tr key={uuidv4()}>
+                <td>{index + 1}</td>
+                <td>
+                  {JSON.stringify(step.listBeforePass)}
+                </td>
+                <td>{String(step.valueBeingInserted)}</td>
+                <td>
+                  {JSON.stringify(step.numberListAfterRemovingValueBeingInserted)}
+                </td>
+                <td>
+                  {step.process.reduce((acc, status) => acc.concat([status, <br key={uuidv4()} />]), [])}
+                </td>
+                <td>
+                  {JSON.stringify(step.listAfterPass)}
+                </td>
               </tr>
-            </thead>
-            <tbody>
-              {returnedApiData.iterations.map((step, index) => (
-                <tr key={uuidv4()}>
-                  <td>{index + 1}</td>
-                  <td>
-                    [
-                    {String(step.listBeforePass)}
-                    ]
-                  </td>
-                  <td>{String(step.valueBeingInserted)}</td>
-                  <td>
-                    [
-                    {String(step.numberListAfterRemovingValueBeingInserted)}
-                    ]
-                  </td>
-                  <td>
-                    {step.process.reduce((acc, status) => acc.concat([status, <br key={uuidv4()} />]), [])}
-                  </td>
-                  <td>
-                    [
-                    {String(step.listAfterPass)}
-                    ]
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-          <h4>
-            End: [
-            {String(returnedApiData.sortedList)}
-            ]
-          </h4>
-        </div>
+            ))}
+          </tbody>
+        </table>
       );
+      break;
     case SortingAlgorithmType.SELECTION_SORT.name:
-      return (
-        <div>
-          <h4>
-            Start: [
-            {String(returnedApiData.unsortedList)}
-            ]
-          </h4>
-          <table className="table">
-            <thead>
-              <tr>
-                <th scope="col">Pass</th>
-                <th scope="col">List Before Pass</th>
-                <th scope="col">Index To Have Sorted Value Assigned To</th>
-                <th scope="col">Internal Process</th>
-                <th scope="col">List After Pass</th>
+      correctAlgorithmProcessDisplay = (
+        <table className="table table-bordered">
+          <thead className="table-active">
+            <tr>
+              <th scope="col">Pass</th>
+              <th scope="col">List Before Pass</th>
+              <th scope="col">Index To Have Sorted Value Assigned To</th>
+              <th scope="col">Internal Process</th>
+              <th scope="col">List After Pass</th>
+            </tr>
+          </thead>
+          <tbody>
+            {returnedApiData.iterations.map((step, index) => (
+              <tr key={uuidv4()}>
+                <td>{index + 1}</td>
+                <td>
+                  {JSON.stringify(step.listBeforePass)}
+                </td>
+                <td>{String(step.indexToHaveSortedValueAssigned)}</td>
+                <td>
+                  {step.process.reduce((acc, status) => acc.concat([status, <br key={uuidv4()} />]), [])}
+                </td>
+                <td>
+                  {JSON.stringify(step.listAfterPass)}
+                </td>
               </tr>
-            </thead>
-            <tbody>
-              {returnedApiData.iterations.map((step, index) => (
-                <tr key={uuidv4()}>
-                  <td>{index + 1}</td>
-                  <td>
-                    [
-                    {String(step.listBeforePass)}
-                    ]
-                  </td>
-                  <td>{String(step.indexToHaveSortedValueAssigned)}</td>
-                  <td>
-                    {step.process.reduce((acc, status) => acc.concat([status, <br key={uuidv4()} />]), [])}
-                  </td>
-                  <td>
-                    [
-                    {String(step.listAfterPass)}
-                    ]
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-          <h4>
-            End: [
-            {String(returnedApiData.sortedList)}
-            ]
-          </h4>
-        </div>
+            ))}
+          </tbody>
+        </table>
       );
+      break;
     default:
       return undefined;
   }
+  return (
+    <div>
+      <h4>
+        Start:
+        {' '}
+        {JSON.stringify(returnedApiData.unsortedList)}
+      </h4>
+      {correctAlgorithmProcessDisplay}
+      <h4>
+        End:
+        {' '}
+        {JSON.stringify(returnedApiData.sortedList)}
+      </h4>
+    </div>
+  );
 }
 
 function SortingAlgorithmProcess(sortingAlgorithmName, returnedApiData) {
-  console.log(returnedApiData, '<<< returnedApiData');
   const sortingAlgorithmProcess = returnedApiData.sortedList.length === 0
     ? ''
     : renderCorrectAlgorithmProcessDisplay(sortingAlgorithmName, returnedApiData);

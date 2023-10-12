6c74182e7eb0752cca2ead5efc507cdfdaa8b334
rvallely <rosievallely@hotmail.com>
2023-10-12 18:45:14 +0100

    update env

--- src/env.js
+++ src/env.js
@@ -1,7 +1,2 @@
-const REACT_APP_ENV = process.env.REACT_APP_ENV || 'dev';
-const REACT_APP_SERVER_URL = process.env.REACT_APP_SERVER_URL;
-
-export default {
-    REACT_APP_ENV,
-    REACT_APP_SERVER_URL
-};
+export const REACT_APP_ENV = process.env.REACT_APP_ENV || 'dev';
+export const REACT_APP_SERVER_URL = process.env.REACT_APP_SERVER_URL;

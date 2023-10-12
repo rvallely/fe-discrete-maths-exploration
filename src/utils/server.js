f5840d2e76bfa0424d6e94feb67890cd06c894cf
rvallely <rosievallely@hotmail.com>
2023-10-12 21:20:34 +0100

    remove log now issue solved

--- src/utils/server.js
+++ src/utils/server.js
@@ -1,7 +1,5 @@
 import axios from 'axios';
 
-console.log(process.env.REACT_APP_ENV, '<<< process.env.REACT_APP_ENV');
-
 const discreteMathsServer = axios.create({
     baseURL: process.env.REACT_APP_ENV === 'prod' ? process.env.REACT_APP_SERVER_URL : 'http://localhost:9090/',
     headers: { 'Access-Control-Allow-Origin': '*' }
